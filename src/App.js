276d193d8cda55c0b7f32244cec4119c425a6dad
NicaHenkel <nica.henkelcop@gmail.com>
2022-07-12 21:28:07 -0300

    Ahora si todo corregido

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import ItemListContainer from "./components/ItemListContainer";
 import ItemCount from "./components/itemCount";
 
 function App() {
+  function handleOnAdd (count){
+    alert(`Se agrego ${count} al carrito de compras`)
+  }
   return (
     <div className="App">
       <Navbar/>
       <ItemListContainer greeting= "Aca van a ir las imagenes de los productos"/>
-      <ItemCount stock= {5} initial={1} onAdd/> 
-      <ItemCount stock= {10} initial={1} onAdd/> 
+      <ItemCount stock= {5} initial={1} onAdd= {handleOnAdd}/> 
+      <ItemCount stock= {10} initial={1} onAdd= {handleOnAdd}/> 
     </div>
   );
 }
